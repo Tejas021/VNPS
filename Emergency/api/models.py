ac046322814b52a271e032922e28f0b4c0981a1c
TeamBlackMamba <teamblackmamba21@gmail.com>
2021-05-09 22:42:49 +0530

    second commit

--- Emergency/api/models.py
+++ Emergency/api/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.db.models.fields import DateField
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
+class UserIn(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    aadhaarno = models.IntegerField()
+    
 class PoliceEmergency(models.Model):
     name = models.CharField(max_length=255)
-    phone = models.IntegerField(max_length=11)
+    phone = models.IntegerField()
     complain = models.CharField(max_length=500)
     date = models.DateField(max_length=12)
